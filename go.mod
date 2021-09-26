a8cbc644a5746945bc78db5462331c2725efaf7c
Simoncao <13671707129@126.com>
2021-09-26 11:57:03 +0800

    [feat]complete

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goWorkSpaceAPI
 go 1.16
 
 require (
+	github.com/go-ego/gpy v0.40.5
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
 	google.golang.org/api v0.57.0
 )
--- main.go
+++ main.go
@@ -2,33 +2,55 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os/exec"
+	"strings"
 
 	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
 	admin "google.golang.org/api/admin/directory/v1"
 	"google.golang.org/api/option"
 )
 
 var credentails string = "../gsuiteServiceAccount.json"
+var password string = "../createRandPwd.sh"
 
 var (
-	method     = flag.String("m", "list", "方法名\nlist 列出域下的所有用户\nupload 创建用户\n")
-	adminEmail = flag.String("a", "", "管理员账号 (默认为空)")
+	method       = flag.String("m", "list", "方法名\nlist 列出域下的所有用户\nupload 创建用户\n")
+	adminEmail   = flag.String("a", "", "管理员账号 (默认为空)")
+	firstName    = flag.String("f", "", "全名 (默认为空)")
+	lastName     = flag.String("l", "", "姓氏 (默认为空)")
+	primaryEmail = flag.String("p", "", "主邮箱 (默认为空)")
+	group        = flag.String("g", "", "分组 (默认为空)")
+	OU           = flag.String("o", "", "组织名 (默认为空)")
 )
 
-func getDirectoryService(adminEmail string) (*admin.Service, error) {
+func getDirectoryService(adminEmail string, mod string) (*admin.Service, error) {
 	ctx := context.Background()
 	jsonCredentials, err := ioutil.ReadFile(credentails)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read credentials: %w", err)
 	}
 
-	config, err := google.JWTConfigFromJSON(jsonCredentials, admin.AdminDirectoryUserScope)
-	if err != nil {
-		return nil, fmt.Errorf("failed to JWTConfigFromJSON: %w", err)
+	var config *jwt.Config
+
+	switch mod {
+	case "user":
+		config, err = google.JWTConfigFromJSON(jsonCredentials, admin.AdminDirectoryUserScope)
+		if err != nil {
+			return nil, fmt.Errorf("failed to JWTConfigFromJSON: %w", err)
+		}
+	case "group":
+		config, err = google.JWTConfigFromJSON(jsonCredentials, admin.AdminDirectoryGroupScope)
+		if err != nil {
+			return nil, fmt.Errorf("failed to JWTConfigFromJSON: %w", err)
+		}
+	default:
+		log.Panicf("mod %s error ", mod)
 	}
 
 	config.Subject = adminEmail
@@ -42,7 +64,7 @@ func getDirectoryService(adminEmail string) (*admin.Service, error) {
 	return srv, nil
 }
 
-func List(srv *admin.Service) {
+func listUser(srv *admin.Service) {
 	r, err := srv.Users.List().Domain("17996.com").OrderBy("email").Do()
 	if err != nil {
 		log.Panicf("failed to list user in domain: %v", err)
@@ -58,18 +80,78 @@ func List(srv *admin.Service) {
 	}
 }
 
-func main() {
-	flag.Parse()
-	srv, err := getDirectoryService(*adminEmail)
+func createUser(srv *admin.Service) {
+
+	//生成随机密码
+	cmd := exec.Command("sh", password)
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Panicf("failed to get service: %v", err)
+		log.Panicf("failed to get password stdout: %v", err)
 	}
+	defer stdout.Close()
+	if err := cmd.Start(); err != nil {
+		log.Panicf("failed to create password: %v", err)
+	}
+	opBytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		log.Panicf("failed to read stdout: %v", err)
+	}
+	password := strings.Split(string(opBytes), "\n")[0]
+
+	//通过json unmarshal
+	//创建 admin.user struct
+	primaryEmail := *primaryEmail + "@17996.com"
+	userJson := fmt.Sprintf(`{"name":{"GivenName":"%s","FamilyName":"%s"},"primaryEmail":"%s","Password":"%s","ChangePasswordAtNextLogin":false,"OrgUnitPath":"%s"}`, *firstName, *lastName, primaryEmail, password, *OU)
+	userByte := []byte(userJson)
+	u := admin.User{}
+	err = json.Unmarshal(userByte, &u)
+	if err != nil {
+		log.Panicf("json unmarshal error: %v", err)
+	}
+
+	//创建用户
+	_, err = srv.Users.Insert(&u).Do()
+	if err != nil {
+		log.Panicf("failed to create user %s: %v", u.Name.FamilyName, err)
+	}
+	log.Printf("用户创建成功！用户名: %s   密码: %s", u.PrimaryEmail, u.Password)
+}
+
+func update(srv *admin.Service) {
+	//添加用户到组
+	m := admin.Member{}
+	m.Email = *primaryEmail + "@17996.com"
+	group := *group + "@17996.com"
+	_, err := srv.Members.Insert(group, &m).Do()
+	if err != nil {
+		log.Panicf("failed to add user %s to group %s: %v", m.Email, group, err)
+	}
+	log.Printf("用户 %s 成功添加到组 %s", m.Email, group)
+
+}
+
+func main() {
+	//解析参数
+	flag.Parse()
+
+	userSrv, err := getDirectoryService(*adminEmail, "user")
+	if err != nil {
+		log.Panicf("failed to get user service: %v", err)
+	}
+
+	groupSrv, err := getDirectoryService(*adminEmail, "group")
+	if err != nil {
+		log.Panicf("failed to get group service: %v", err)
+	}
+
 	switch *method {
 	case "list":
-		List(srv)
+		listUser(userSrv)
 	case "create":
-		fmt.Println("coming soon...")
+		createUser(userSrv)
+		update(groupSrv)
 	default:
 		log.Panic("method error")
 	}
+
 }
